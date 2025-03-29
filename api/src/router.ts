ab4208f9e126cb2c83a5260d289a53017b6dec52
Alberto Beltran <albertobeltran@protonmail.com>
2025-03-29 08:21:14 -0600

    API para interactuar con Gemini

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { helloWorld } from './controllers/index.js';
+import { postMessage } from './controllers/chatbot.js';
 
 export const router = Router();
 
 router.get('/', helloWorld);
+router.post('/message', postMessage);
