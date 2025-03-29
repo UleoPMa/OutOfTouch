1128905b17f3eafdbec531bc109703d251031c71
juliana07 <ednajuliana093@gmail.com>
2025-03-29 03:04:11 -0600

    implemente endpoint para correos v1

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { helloWorld } from './controllers/index.js';
+const { enviar } = require('./controllers/correo');
 
 export const router = Router();
 
 router.get('/', helloWorld);
+router.post('/enviar-correo', enviar);
